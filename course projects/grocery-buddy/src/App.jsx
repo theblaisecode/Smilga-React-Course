23ac26f318b03a3ecadb234727f7d341751d934e
theblaisecode <theblaisecode@gmail.com>
2024-06-12 11:04:23 +0100

    first commit

--- course projects/grocery-buddy/src/App.jsx
+++ course projects/grocery-buddy/src/App.jsx
@@ -4,6 +4,7 @@ import Form from "./components/Form";
 import GroceryList from "./components/GroceryList";
 
 function App() {
+  // Default shopping list
   const [isList, setIsList] = useState([
     "Pet goat",
     "Tesla",
@@ -14,29 +15,34 @@ function App() {
     "The Moon",
   ]);
 
+  // Input
   const [isGrocery, setIsGrocery] = useState({
     groceryItem: "",
   });
 
+  // checkbox state
   const [isChecked, setIsChecked] = useState({});
 
+  // Get info from the input
   const getDetails = (e) => {
     setIsGrocery({
       [e.target.name]: e.target.value,
     });
   };
 
+  // If checkbox is checked
   function checkedBox(index) {
-    console.log(index);
     setIsChecked((prevChecked) => ({
       ...prevChecked,
       [index]: !prevChecked[index],
     }));
   }
 
+  // Add Item to shopping list
   const submitForm = (e) => {
     e.preventDefault();
 
+    // Error popup if input is empty
     if (isGrocery.groceryItem === "") {
       toast.error("Please enter an item", {
         position: "top-center",
@@ -44,24 +50,28 @@ function App() {
       return;
     }
 
+    // Add new item to shopping list
     setIsList((prevList) => {
       return [...prevList, isGrocery.groceryItem];
     });
 
+    // Successfully added to list popup
     toast.success("Item added to the list!", {
       position: "top-center",
     });
 
+    // Clear input after every addition to list
     setIsGrocery({ groceryItem: "" });
   };
 
-  console.log(isList);
-
+  // Delete item from list
   const deleteItem = (index) => {
+    // Delete item
     setIsList((isPrevList) =>
       isPrevList.filter((item, remove) => remove !== index)
     );
 
+    // Set checked state to not automatically add to the next item when the top item is deleted
     setIsChecked((prevChecked) => {
       const newChecked = {};
       for (let i = 0; i < isList.length; i++) {
@@ -74,6 +84,7 @@ function App() {
       return newChecked;
     });
 
+    // Deleted item popup
     toast.info("Item Deleted", {
       position: "top-center",
     });
@@ -98,6 +109,7 @@ function App() {
         />
       )}
 
+      {/* popup */}
       <ToastContainer />
     </main>
   );
