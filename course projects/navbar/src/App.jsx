db2b742596b86c161dc3b725da5e7230fbf40124
theblaisecode <theblaisecode@gmail.com>
2024-06-14 10:50:07 +0100

    first commit

---
+++ course projects/navbar/src/App.jsx
@@ -0,0 +1,12 @@
+import Navbar from "./components/Navbar";
+
+function App() {
+  return (
+    <main>
+      <h1>Hello</h1>
+      <Navbar />
+    </main>
+  );
+}
+
+export default App;
---
+++ course projects/navbar/src/data.jsx
@@ -0,0 +1,50 @@
+export const links = [
+  {
+    id: 1,
+    url: "/",
+    text: "home",
+  },
+  {
+    id: 2,
+    url: "/about",
+    text: "about",
+  },
+  {
+    id: 3,
+    url: "/projects",
+    text: "projects",
+  },
+  {
+    id: 4,
+    url: "/contact",
+    text: "contact",
+  },
+  {
+    id: 5,
+    url: "/profile",
+    text: "profile",
+  },
+];
+
+export const social = [
+  {
+    id: 1,
+    url: "https://www.twitter.com/theblaisecode",
+    icon: "fa-brands fa-square-twitter fa-2x",
+  },
+  {
+    id: 2,
+    url: "https://www.github.com/theblaisecode",
+    icon: "fa-brands fa-square-github fa-2x",
+  },
+  {
+    id: 3,
+    url: "https://www.linkedin.com/theblaisecode",
+    icon: "fa-brands fa-linkedin fa-2x",
+  },
+  {
+    id: 4,
+    url: "https://www.instagram.com/theblaisecode",
+    icon: "fa-brands fa-square-instagram fa-2x",
+  },
+];
