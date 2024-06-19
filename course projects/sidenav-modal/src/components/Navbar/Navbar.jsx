cd295907ea150561d66053c4a3ce55487b8d8273
theblaisecode <theblaisecode@gmail.com>
2024-06-19 17:03:48 +0100

    first commit

---
+++ course projects/sidenav-modal/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,66 @@
+import { useState } from "react";
+import { links, social } from "../../data";
+import logo from "../../img/TheBlaiseCodewhite.png";
+import "./Navbar.css";
+
+function Navbar() {
+  const [isMobile, setIsMobile] = useState(false);
+
+  function switchMobile() {
+    setIsMobile((prevIsMobile) => !prevIsMobile);
+  }
+
+  return (
+    <nav>
+      <div className="container">
+        <button onClick={() => switchMobile()}>
+          <i className={"fa-solid fa-bars fa-xl"}></i>
+        </button>
+
+        <div className={isMobile ? "navContent active" : "navContent"}>
+          <div className="logo">
+            <img src={logo} alt="TheBlaiseCode logo" />
+            <button onClick={() => switchMobile()}>
+              <i className="fa-solid fa-xmark fa-xl"></i>
+            </button>
+          </div>
+
+          <div className="allLinks">
+            <ul className="pages">
+              {links.map((pages) => {
+                console.log(pages);
+                const { id, url, text, aria, icon } = pages;
+                console.log(icon, text);
+
+                return (
+                  <li key={id}>
+                    <a href={url} aria-label={aria}>
+                      {icon}
+                      {text}
+                    </a>
+                  </li>
+                );
+              })}
+            </ul>
+
+            <ul className="socials">
+              {social.map((socialLinks) => {
+                const { id, url, icon, aria } = socialLinks;
+
+                return (
+                  <li key={id}>
+                    <a href={url} target="_blank" aria-label={aria}>
+                      {icon}
+                    </a>
+                  </li>
+                );
+              })}
+            </ul>
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
