91a48b0c2be403a12742cfdd01a94c8ba2fc3de6
theblaisecode <theblaisecode@gmail.com>
2024-06-22 20:00:27 +0100

    first commit

--- 03 - React Hooks and Advance Topics/my-app/src/App.jsx
+++ 03 - React Hooks and Advance Topics/my-app/src/App.jsx
@@ -31,7 +31,8 @@
 // import UseRefBasics from "./tutorial/07-useRef/starter/01-useRef-basics";
 // import ToggleExample from "./tutorial/08-custom-hooks/starter/01-toggle";
 // import FetchData from "./tutorial/08-custom-hooks/starter/02-fetch-data";
-import Navbar from "./tutorial/09-context-api/starter/Navbar";
+// import Navbar from "./tutorial/09-context-api/starter/Navbar";
+import ReducerBasics from "./tutorial/10-useReducer/starter/01-useReducer";
 
 function App() {
   return (
@@ -68,7 +69,8 @@ function App() {
       {/* <UseRefBasics /> */}
       {/* <ToggleExample /> */}
       {/* <FetchData /> */}
-      <Navbar />
+      {/* <Navbar /> */}
+      <ReducerBasics />
     </div>
   );
 }
--- 03 - React Hooks and Advance Topics/my-app/src/tutorial/10-useReducer/starter/01-useReducer.jsx
+++ 03 - React Hooks and Advance Topics/my-app/src/tutorial/10-useReducer/starter/01-useReducer.jsx
@@ -1,30 +1,74 @@
-import React from 'react';
-import { data } from '../../../data';
+import { useReducer } from "react";
+import { data } from "../../../data";
+
+const defaultState = {
+  people: data,
+  isLoading: false,
+};
+
+const CLEAR_LIST = "CLEAR_LIST";
+const RESET_LIST = "RESET_LIST";
+const REMOVE_ITEM = "REMOVE_ITEM";
+
+const reducer = (state, action) => {
+  if (action.type === CLEAR_LIST) {
+    return { ...state, people: [] };
+  } else if (action.type === RESET_LIST) {
+    return { ...state, people: data };
+  } else if (action.type === REMOVE_ITEM) {
+    return {
+      ...state,
+      people: state.people.filter((person) => person.id !== action.payload.id),
+    };
+  }
+  // return state
+  throw new Error(`No matiching "${action.type}" - action type`);
+};
+
 const ReducerBasics = () => {
-  const [people, setPeople] = React.useState(data);
+  const [state, dispatch] = useReducer(reducer, defaultState);
 
   const removeItem = (id) => {
-    let newPeople = people.filter((person) => person.id !== id);
-    setPeople(newPeople);
+    dispatch({ type: "REMOVE_ITEM", payload: { id } });
   };
+
+  const clearList = () => {
+    dispatch({ type: "CLEAR_LIST" });
+  };
+
+  const resetList = () => {
+    dispatch({ type: "RESET_LIST" });
+  };
+
   return (
     <div>
-      {people.map((person) => {
+      {state.people.map((person) => {
         const { id, name } = person;
+
         return (
-          <div key={id} className='item'>
+          <div key={id} className="item">
             <h4>{name}</h4>
+
             <button onClick={() => removeItem(id)}>remove</button>
           </div>
         );
       })}
-      <button
-        className='btn'
-        style={{ marginTop: '2rem' }}
-        onClick={() => setPeople([])}
-      >
-        clear items
-      </button>
+
+      {state.people.length === 0 ? (
+        <button
+          className="btn"
+          style={{ marginTop: "2rem" }}
+          onClick={resetList}>
+          Reset
+        </button>
+      ) : (
+        <button
+          className="btn"
+          style={{ marginTop: "2rem" }}
+          onClick={clearList}>
+          clear items
+        </button>
+      )}
     </div>
   );
 };
