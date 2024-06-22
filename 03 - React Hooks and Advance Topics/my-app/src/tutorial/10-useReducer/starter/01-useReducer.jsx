879647f51edea0671dc8303fb28376deac6a95e6
theblaisecode <theblaisecode@gmail.com>
2024-06-22 20:08:57 +0100

    first commit

--- 03 - React Hooks and Advance Topics/my-app/src/tutorial/10-useReducer/starter/01-useReducer.jsx
+++ 03 - React Hooks and Advance Topics/my-app/src/tutorial/10-useReducer/starter/01-useReducer.jsx
@@ -1,43 +1,26 @@
 import { useReducer } from "react";
 import { data } from "../../../data";
+import { CLEAR_LIST, RESET_LIST, REMOVE_ITEM } from "./actions";
+import reducer from "./reducer";
 
 const defaultState = {
   people: data,
   isLoading: false,
 };
 
-const CLEAR_LIST = "CLEAR_LIST";
-const RESET_LIST = "RESET_LIST";
-const REMOVE_ITEM = "REMOVE_ITEM";
-
-const reducer = (state, action) => {
-  if (action.type === CLEAR_LIST) {
-    return { ...state, people: [] };
-  } else if (action.type === RESET_LIST) {
-    return { ...state, people: data };
-  } else if (action.type === REMOVE_ITEM) {
-    return {
-      ...state,
-      people: state.people.filter((person) => person.id !== action.payload.id),
-    };
-  }
-  // return state
-  throw new Error(`No matiching "${action.type}" - action type`);
-};
-
 const ReducerBasics = () => {
   const [state, dispatch] = useReducer(reducer, defaultState);
 
   const removeItem = (id) => {
-    dispatch({ type: "REMOVE_ITEM", payload: { id } });
+    dispatch({ type: REMOVE_ITEM, payload: { id } });
   };
 
   const clearList = () => {
-    dispatch({ type: "CLEAR_LIST" });
+    dispatch({ type: CLEAR_LIST });
   };
 
   const resetList = () => {
-    dispatch({ type: "RESET_LIST" });
+    dispatch({ type: RESET_LIST });
   };
 
   return (
---
+++ 03 - React Hooks and Advance Topics/my-app/src/tutorial/10-useReducer/starter/reducer.js
@@ -0,0 +1,21 @@
+import { CLEAR_LIST, RESET_LIST, REMOVE_ITEM } from "./actions";
+import { data } from "../../../data";
+
+
+const reducer = (state, action) => {
+  if (action.type === CLEAR_LIST) {
+    return { ...state, people: [] };
+  } else if (action.type === RESET_LIST) {
+    return { ...state, people: data };
+  } else if (action.type === REMOVE_ITEM) {
+    return {
+      ...state,
+      people: state.people.filter((person) => person.id !== action.payload.id),
+    };
+  }
+  // return state
+  throw new Error(`No matiching "${action.type}" - action type`);
+};
+
+
+export default reducer
