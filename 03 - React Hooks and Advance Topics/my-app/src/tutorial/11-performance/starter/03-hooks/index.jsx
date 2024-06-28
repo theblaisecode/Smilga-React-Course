f86a2ce2b702e9c064314399d87a08b09b557fad
theblaisecode <theblaisecode@gmail.com>
2024-06-28 15:03:47 +0100

    first commit

--- 03 - React Hooks and Advance Topics/my-app/src/tutorial/11-performance/starter/03-hooks/index.jsx
+++ 03 - React Hooks and Advance Topics/my-app/src/tutorial/11-performance/starter/03-hooks/index.jsx
@@ -6,6 +6,11 @@ const LowerState = () => {
   const [people, setPeople] = useState(data);
   const [count, setCount] = useState(0);
 
+  const removePerson = (id) => {
+    const newPeople = people.filter((person) => person.id !== id);
+    setPeople(newPeople);
+  };
+
   return (
     <section>
       <button
@@ -16,7 +21,7 @@ const LowerState = () => {
         count {count}
       </button>
 
-      <List people={people} />
+      <List people={people} removePerson={removePerson}/>
     </section>
   );
 };
