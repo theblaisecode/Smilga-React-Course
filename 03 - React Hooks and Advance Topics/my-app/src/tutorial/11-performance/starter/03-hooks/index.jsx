824ca28057f59fd21702e469780e5146e7504a86
theblaisecode <theblaisecode@gmail.com>
2024-06-29 17:53:56 +0100

    first commit

--- 03 - React Hooks and Advance Topics/my-app/src/tutorial/11-performance/starter/03-hooks/index.jsx
+++ 03 - React Hooks and Advance Topics/my-app/src/tutorial/11-performance/starter/03-hooks/index.jsx
@@ -1,27 +1,29 @@
-import { useState } from 'react';
-import { data } from '../../../../data';
-import List from './List';
+import { useCallback, useState } from "react";
+import { data } from "../../../../data";
+import List from "./List";
 
 const LowerState = () => {
   const [people, setPeople] = useState(data);
   const [count, setCount] = useState(0);
 
-  const removePerson = (id) => {
-    const newPeople = people.filter((person) => person.id !== id);
-    setPeople(newPeople);
-  };
+  const removePerson = useCallback(
+    (id) => {
+      const newPeople = people.filter((person) => person.id !== id);
+      setPeople(newPeople);
+    },
+    [people]
+  );
 
   return (
     <section>
       <button
-        className='btn'
+        className="btn"
         onClick={() => setCount(count + 1)}
-        style={{ marginBottom: '1rem' }}
-      >
+        style={{ marginBottom: "1rem" }}>
         count {count}
       </button>
 
-      <List people={people} removePerson={removePerson}/>
+      <List people={people} removePerson={removePerson} />
     </section>
   );
 };
