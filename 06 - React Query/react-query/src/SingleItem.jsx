a86c2aae3b3c36399a26ce51885eb9ffe3775dff
theblaisecode <theblaisecode@gmail.com>
2024-07-08 12:48:19 +0100

    first commit

--- 06 - React Query/react-query/src/SingleItem.jsx
+++ 06 - React Query/react-query/src/SingleItem.jsx
@@ -1,27 +1,28 @@
 const SingleItem = ({ item }) => {
   return (
-    <div className='single-item'>
+    <div className="single-item">
       <input
-        type='checkbox'
+        type="checkbox"
         checked={item.isDone}
-        onChange={() => console.log('edit task')}
+        onChange={() => console.log("edit task")}
       />
+
       <p
         style={{
-          textTransform: 'capitalize',
-          textDecoration: item.isDone && 'line-through',
-        }}
-      >
+          textTransform: "capitalize",
+          textDecoration: item.isDone && "line-through",
+        }}>
         {item.title}
       </p>
+
       <button
-        className='btn remove-btn'
-        type='button'
-        onClick={() => console.log('delete task')}
-      >
+        className="btn remove-btn"
+        type="button"
+        onClick={() => console.log("delete task")}>
         delete
       </button>
     </div>
   );
 };
+
 export default SingleItem;
