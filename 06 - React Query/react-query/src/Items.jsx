8d8dab7a7d05ea3896d2e25e78b531bc1d70c5a5
theblaisecode <theblaisecode@gmail.com>
2024-07-10 11:53:03 +0100

    first commit

--- 06 - React Query/react-query/src/Items.jsx
+++ 06 - React Query/react-query/src/Items.jsx
@@ -1,6 +1,15 @@
 import SingleItem from "./SingleItem";
+import { useQuery } from "@tanstack/react-query";
+import customInstance from "./utils";
 
 const Items = ({ items }) => {
+  const result = useQuery({
+    queryKey: ["tasks"],
+    queryFn: () => customInstance.get("/"),
+  });
+  
+  console.log(result);
+
   return (
     <div className="items">
       {items.map((item) => {
