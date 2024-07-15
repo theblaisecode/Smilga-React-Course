5e998341f1c7766ebe8e6f75758ec8cabc3a2c63
theblaisecode <theblaisecode@gmail.com>
2024-07-15 09:26:43 +0100

    first commit

--- 06 - React Query/react-query/src/reactQueryAndCustomHooks.jsx
+++ 06 - React Query/react-query/src/reactQueryAndCustomHooks.jsx
@@ -1,6 +1,5 @@
-import { useQuery } from "@tanstack/react-query";
 import { useState } from "react";
-import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { useQuery, useMutation, useQueryClient } from "@tanstack/react-query";
 import { toast } from "react-toastify";
 import customInstance from "./utils";
 
