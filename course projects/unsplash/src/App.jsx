2db01f7ce2f0aaa8a24ea9fdcb2a84a1b4a64654
theblaisecode <theblaisecode@gmail.com>
2024-07-18 06:31:01 +0100

    first commit

--- course projects/unsplash/src/App.jsx
+++ course projects/unsplash/src/App.jsx
@@ -4,6 +4,7 @@ import Header from "./components/Header/Header";
 import { useGlobalContext } from "./components/GlobalContext";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import Footer from "./components/Footer/Footer";
 
 function App() {
   const { isDarkMode, toggleTheme, searchQuery, searchNewImages } =
@@ -14,6 +15,7 @@ function App() {
       <Header isDarkMode={isDarkMode} toggleTheme={toggleTheme} />
       <Form isDarkMode={isDarkMode} onSearch={searchNewImages} />
       <Gallery isDarkMode={isDarkMode} searchQuery={searchQuery} />
+      <Footer isDarkMode={isDarkMode}/>
       <ToastContainer position="top-center" />
     </>
   );
