fef73be95bad9f512acfaf041789a286ef647d7f
theblaisecode <theblaisecode@gmail.com>
2024-07-22 14:35:40 +0100

    first commit

--- course projects/contentful/src/components/Projects/Projects.jsx
+++ course projects/contentful/src/components/Projects/Projects.jsx
@@ -1,8 +1,29 @@
-import { projects } from "../../data";
+import { useGetProjects } from "../getProjects";
 import "../getProjects";
 import "./Projects.css";
 
 function Projects() {
+  const { isLoading, projects } = useGetProjects();
+
+  if (isLoading) {
+    return (
+      <section id="projects">
+        <div className="container">
+          <div className="projectsContent">
+            <div className="contentTop">
+              <h2 className="title">Projects</h2>
+              <div className="underline"></div>
+            </div>
+
+            <div className="contentBottom">
+              <h2>Loading...</h2>
+            </div>
+          </div>
+        </div>
+      </section>
+    );
+  }
+
   return (
     <section id="projects">
       <div className="container">
@@ -13,13 +34,18 @@ function Projects() {
           </div>
 
           <div className="contentBottom">
-            {projects.map((item, index) => {
-              const { title, image, url } = item;
+            {projects.map((item) => {
+              const { id, title, imgs, url } = item;
               console.log(item);
 
               return (
-                <a className="card" key={index} href={url}>
-                  <img src={image} alt={title} />
+                <a
+                  className="card"
+                  key={id}
+                  href={url}
+                  target="_blank"
+                  rel="noreferrer">
+                  <img src={imgs} alt={title} />
                   <p>{title}</p>
                 </a>
               );
--- course projects/contentful/src/components/getProjects.jsx
+++ course projects/contentful/src/components/getProjects.jsx
@@ -1,15 +1,42 @@
+import { useEffect, useState } from "react";
 import { createClient } from "contentful";
 
-const cliet = createClient({
+const client = createClient({
   space: "158b8ogzx9gn",
   environment: "master",
   accessToken: "17SU7joH1oTPhwV647LAXDETuMsaqpjCNHArgmmteVk",
 });
 
-cliet
-  .getEntries({ content_type: "projects" })
-  .then((response) => console.log(response));
+export const useGetProjects = () => {
+  const [isLoading, setIsLoading] = useState(true);
+  const [projects, setProjects] = useState([]);
 
-const usegetProjects = () => {
-  
-}
+  async function getData() {
+    try {
+      const res = await client.getEntries({ content_type: "projects" });
+      const projects = res.items.map((item) => {
+        console.log(item);
+        const { title, url, img } = item.fields;
+        const id = item.sys.id;
+        const imgs = img?.fields?.file?.url;
+        return { title, url, id, imgs };
+      });
+
+      const reversedProjects = projects.reverse();
+
+      setProjects(reversedProjects);
+      setIsLoading(false);
+    } catch (error) {
+      console.log(error);
+      setIsLoading(false);
+    }
+  }
+
+  useEffect(() => {
+    getData();
+  }, []);
+
+  return { isLoading, projects };
+};
+
+client.getEntries({ content_type: "projects" }).then((res) => console.log(res));
