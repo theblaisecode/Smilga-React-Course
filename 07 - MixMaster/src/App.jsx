c8cb3ab0d0597cf1ebfb33cc54b3f3acec3e1de6
theblaisecode <theblaisecode@gmail.com>
2024-08-14 16:43:36 +0100

    first commit

--- 07 - MixMaster/src/App.jsx
+++ 07 - MixMaster/src/App.jsx
@@ -26,4 +26,5 @@ const router = createBrowserRouter([
 const App = () => {
   return <RouterProvider router={router} />;
 };
+
 export default App;
--- 07 - MixMaster/src/pages/Landing/Landing.jsx
+++ 07 - MixMaster/src/pages/Landing/Landing.jsx
@@ -1,4 +1,3 @@
-import React from "react";
 import "./Landing.css";
 
 function Landing() {
