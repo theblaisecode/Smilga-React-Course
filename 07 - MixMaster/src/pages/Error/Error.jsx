d94ee7caaff21d1df764ec6ff6ebcfce173a807c
theblaisecode <theblaisecode@gmail.com>
2024-08-15 09:24:57 +0100

    first commit

--- 07 - MixMaster/src/pages/Error/Error.jsx
+++ 07 - MixMaster/src/pages/Error/Error.jsx
@@ -3,9 +3,25 @@ import img from "../../assets/not-found.svg";
 import ErrorWrapper from "./Error.js";
 
 function Error() {
+  const error = useRouteError();
+  console.log(error);
+
+  if (error.status === 404) {
+    return (
+      <ErrorWrapper>
+        <div>
+          <img src={img} alt="page not found" />
+          <h3>Ohh!</h3>
+          <p>We can't seem to fid the page you are looking for</p>
+          <Link to="/">Back to Home</Link>
+        </div>
+      </ErrorWrapper>
+    );
+  }
+
   return (
     <ErrorWrapper>
-      <h3>Error</h3>
+      <h3>Something went wrong</h3>
     </ErrorWrapper>
   );
 }
