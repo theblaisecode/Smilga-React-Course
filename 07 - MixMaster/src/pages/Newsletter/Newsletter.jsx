4ff6cf1176517cd1885d3921606c0aa496dfd93a
theblaisecode <theblaisecode@gmail.com>
2024-08-19 07:13:06 +0100

    first commit

--- 07 - MixMaster/src/pages/Newsletter/Newsletter.jsx
+++ 07 - MixMaster/src/pages/Newsletter/Newsletter.jsx
@@ -1,10 +1,54 @@
-import "./Newsletter.js";
+import NewsletterWrapper from "./Newsletter.js";
 
 function Newsletter() {
   return (
-    <section id="newsletter">
-      <h3>Newsletter</h3>
-    </section>
+    <NewsletterWrapper>
+      <form action="" className="form">
+        <h4>Our Newslette</h4>
+
+        <div className="form-row">
+          <label htmlFor="name" className="form-label">
+            name
+          </label>
+          <input
+            type="text"
+            className="form-input"
+            name="name"
+            id="name"
+            defaultValue="Blaise"
+            placeholder="Name"
+          />
+        </div>
+
+        <div className="form-row">
+          <label htmlFor="lastName" className="form-label">
+            Last Name
+          </label>
+          <input
+            type="text"
+            className="form-input"
+            name="lastName"
+            id="lastName"
+            defaultValue="Nwachukwu"
+            placeholder="Last Name"
+          />
+        </div>
+
+        <div className="form-row">
+          <label htmlFor="email" className="form-label">
+            email
+          </label>
+          <input
+            type="text"
+            className="form-input"
+            name="email"
+            id="email"
+            defaultValue="theblaiscode@gmail.com"
+            placeholder="Email Address"
+          />
+        </div>
+      </form>
+    </NewsletterWrapper>
   );
 }
 
