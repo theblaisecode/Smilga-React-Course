56df22f12c40e2036848a320f0904959612d5d1d
theblaisecode <theblaisecode@gmail.com>
2024-08-30 07:34:33 +0100

    first commit

--- 07 - MixMaster/src/App.jsx
+++ 07 - MixMaster/src/App.jsx
@@ -1,4 +1,6 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import {
   About,
   Cocktail,
