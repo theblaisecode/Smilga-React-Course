7660072bd3d833cae2e4656050dac922e8d8eabe
theblaisecode <theblaisecode@gmail.com>
2024-09-25 09:23:56 +0100

    first commit

--- 07 - MixMaster/src/App.jsx
+++ 07 - MixMaster/src/App.jsx
@@ -1,6 +1,5 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import {
   About,
   Cocktail,
@@ -23,42 +22,44 @@ const queryClient = new QueryClient({
   },
 });
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <HomeLayout />,
-    errorElement: <Error />,
-    children: [
-      {
-        index: true,
-        element: <Landing />,
-        errorElement: <SinglePageError />,
-        loader: landingLoader(queryClient),
-      },
-      {
-        path: "cocktail/:id",
-        errorElement: <SinglePageError />,
-        loader: singleCocktailLoader(queryClient),
-        element: <Cocktail />,
-      },
-      {
-        path: "newsletter",
-        element: <Newsletter />,
-        action: newsletterAction,
-      },
-      {
-        path: "about",
-        element: <About />,
-      },
-    ],
-  },
-]);
+const router = createBrowserRouter(
+  [
+    {
+      path: "/",
+      element: <HomeLayout />,
+      errorElement: <Error />,
+      children: [
+        {
+          index: true,
+          element: <Landing />,
+          errorElement: <SinglePageError />,
+          loader: landingLoader(queryClient),
+        },
+        {
+          path: "cocktail/:id",
+          errorElement: <SinglePageError />,
+          loader: singleCocktailLoader(queryClient),
+          element: <Cocktail />,
+        },
+        {
+          path: "newsletter",
+          element: <Newsletter />,
+          action: newsletterAction,
+        },
+        {
+          path: "about",
+          element: <About />,
+        },
+      ],
+    },
+  ],
+  { basename: "/Tipsy-Mix" }
+);
 
 const App = () => {
   return (
     <QueryClientProvider client={queryClient}>
-      <RouterProvider router={router} />;
-      <ReactQueryDevtools initialIsOpen={false} />
+      <RouterProvider router={router} />
     </QueryClientProvider>
   );
 };
