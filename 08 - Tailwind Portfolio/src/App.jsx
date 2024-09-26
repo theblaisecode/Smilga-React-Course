5ba6cccc6c1dd7a20a6260fd3908f44e606fc4e5
theblaisecode <theblaisecode@gmail.com>
2024-09-26 14:40:13 +0100

    first commit

--- 08 - Tailwind Portfolio/src/App.jsx
+++ 08 - Tailwind Portfolio/src/App.jsx
@@ -1,11 +1,19 @@
+import About from "./components/About";
+import Footer from "./components/Footer";
 import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
+import Projects from "./components/Projects";
+import Skills from "./components/Skills";
 
 function App() {
   return (
     <>
       <Navbar />
       <Hero />
+      <Skills />
+      <About />
+      <Projects />
+      <Footer />
     </>
   );
 }
--- 08 - Tailwind Portfolio/src/components/Skills.jsx
+++ 08 - Tailwind Portfolio/src/components/Skills.jsx
@@ -1,10 +1,27 @@
+import { skills } from "../data";
+import SectionTitle from "./SectionTitle";
+
+const SkillsCard = ({ icon, title, text }) => {
+  return (
+    <article>
+      <span className="h-16 w-16">{icon}</span>
+      <h4 className="mt-6 font-bold">{title}</h4>
+      <p className="mt-2 text-slate-500">{text}</p>
+    </article>
+  );
+};
+
 function Skills() {
   return (
-    <section id="about" className="bg-emerald-100 py-24">
-      <div className="mx-auto max-w-7xl px-8 grid md:grid-cols-2 items-center gap-8">
-        <div className="contentTop"></div>
+    <section id="skills" className="py-10">
+      <div className="container pt-10">
+        <SectionTitle text="tech stack " />
 
-        <div className="contentBottom "></div>
+        <div className=" py-16 grid md:grid-cols-2 lg:grid-cols-3 gap-8">
+          {skills.map((skill) => {
+            return <SkillsCard key={skill.id} {...skill} />;
+          })}
+        </div>
       </div>
     </section>
   );
