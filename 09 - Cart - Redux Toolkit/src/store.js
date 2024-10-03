8e814767b8bec281fde91e6180294c82ed6c47f2
theblaisecode <theblaisecode@gmail.com>
2024-10-03 08:37:36 +0100

    first commit

--- 09 - Cart - Redux Toolkit/src/store.js
+++ 09 - Cart - Redux Toolkit/src/store.js
@@ -1,6 +1,6 @@
 import { configureStore } from "@reduxjs/toolkit";
-import "./features/cart/cartSlice";
+import cartReducer from "./features/cart/cartSlice";
 
 export const store = configureStore({
-  reducer: {},
+  reducer: { cart: cartReducer },
 });
