350a16785a599450c39ee2d68fe0ee1069853b8f
theblaisecode <theblaisecode@gmail.com>
2024-10-03 08:18:15 +0100

    first commit

--- 09 - Cart - Redux Toolkit/src/main.jsx
+++ 09 - Cart - Redux Toolkit/src/main.jsx
@@ -1,5 +1,11 @@
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
+import { store } from "./store";
+import { Provider } from "react-redux";
 import "./style.css";
 
-createRoot(document.getElementById("root")).render(<App />);
+createRoot(document.getElementById("root")).render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
