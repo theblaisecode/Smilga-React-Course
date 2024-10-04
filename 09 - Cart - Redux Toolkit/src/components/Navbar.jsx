58092ec4f4b990e3212e3b464483edd866ba356b
theblaisecode <theblaisecode@gmail.com>
2024-10-04 19:10:22 +0100

    first commit

--- 09 - Cart - Redux Toolkit/src/components/Navbar.jsx
+++ 09 - Cart - Redux Toolkit/src/components/Navbar.jsx
@@ -9,6 +9,9 @@ function Navbar() {
 
         <div className="nav-container">
           <CartIcon />
+          <div className="amount-container">
+            <p className="total-amount">0</p>
+          </div>
         </div>
       </div>
     </nav>
