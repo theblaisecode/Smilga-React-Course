51134ba51e433a0c6a3dd33225474d08e48181cf
theblaisecode <theblaisecode@gmail.com>
2024-10-04 18:32:55 +0100

    firsst commit

---
+++ 09 - Cart - Redux Toolkit/src/features/modal/modalSlice.js
@@ -0,0 +1,12 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {};
+
+const modalSlice = createSlice({
+  name: "modal",
+  initialState,
+});
+
+console.log(modalSlice);
+
+export default modalSlice;
