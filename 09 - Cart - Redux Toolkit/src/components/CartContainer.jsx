86d88e7d667301a2a0f7624e23e66e6d67f68131
theblaisecode <theblaisecode@gmail.com>
2024-10-08 18:55:28 +0100

    first commit

--- 09 - Cart - Redux Toolkit/src/components/CartContainer.jsx
+++ 09 - Cart - Redux Toolkit/src/components/CartContainer.jsx
@@ -1,7 +1,41 @@
+import { useSelector } from "react-redux";
+import CartItems from "./CartItem";
+
 const CartContainer = () => {
+  const { cartItems, amount, total } = useSelector((store) => store.cart);
+
+  if (amount < 1) {
+    return (
+      <section className="cart">
+        <header>
+          <h2>Your Bag</h2>
+          <h4 className="empty-cart">is currently empty.</h4>
+        </header>
+      </section>
+    );
+  }
+
   return (
-    <section>
-      <div className="container">Cart Container</div>
+    <section className="cart">
+      <h2>Your Bag</h2>
+
+      <div className="container">
+        {cartItems.map((items) => {
+          return <CartItems key={items.id} {...items} />;
+        })}
+      </div>
+
+      <footer>
+        <hr />
+        <div className="cart-total">
+          <h4>
+            total <span>${total}</span>
+          </h4>
+        </div>
+
+        <button className="btn clear-btn">clear cart</button>
+        <button className="btn clear-btn">clear cart</button>
+      </footer>
     </section>
   );
 };
