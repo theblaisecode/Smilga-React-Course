1f436e34f8aac84dd1796cd7dfa344c6eb9be22c
theblaisecode <theblaisecode@gmail.com>
2024-10-08 19:07:16 +0100

    first commit

--- 09 - Cart - Redux Toolkit/src/components/CartItem.jsx
+++ 09 - Cart - Redux Toolkit/src/components/CartItem.jsx
@@ -4,6 +4,24 @@ const CartItems = ({ id, img, title, price, amount }) => {
   return (
     <div className="cart-item">
       <img src={img} alt={title} />
+
+      <div>
+        <h4>{title}</h4>
+
+        <h4 className="item-price">${price}</h4>
+
+        <buttom className="remove-btn">remove</buttom>
+      </div>
+
+      <div>
+        <button className="amount-btn">
+          <ChevronUp />
+        </button>
+        <p className="amount">{amount}</p>
+        <button className="amount-btn">
+          <ChevronDown />
+        </button>
+      </div>
     </div>
   );
 };
