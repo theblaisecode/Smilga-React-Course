55ef644db8a1d0ff0359a3e37c02b18bb0ef4a3a
theblaisecode <theblaisecode@gmail.com>
2024-10-08 19:04:48 +0100

    first commit

--- 09 - Cart - Redux Toolkit/src/components/CartItem.jsx
+++ 09 - Cart - Redux Toolkit/src/components/CartItem.jsx
@@ -1,8 +1,10 @@
-const CartItems = () => {
+import { ChevronDown, ChevronUp } from "../icons";
+
+const CartItems = ({ id, img, title, price, amount }) => {
   return (
-    <section>
-      <div className="container">Cart Items</div>
-    </section>
+    <div className="cart-item">
+      <img src={img} alt={title} />
+    </div>
   );
 };
 
