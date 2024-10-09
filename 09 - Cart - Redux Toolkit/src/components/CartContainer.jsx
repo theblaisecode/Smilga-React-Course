eb0b50e7926669a40e08ddbfd03c9b0c8f685996
theblaisecode <theblaisecode@gmail.com>
2024-10-09 08:04:00 +0100

    first commit

--- 09 - Cart - Redux Toolkit/src/components/CartContainer.jsx
+++ 09 - Cart - Redux Toolkit/src/components/CartContainer.jsx
@@ -9,6 +9,7 @@ const CartContainer = () => {
       <section className="cart">
         <header>
           <h2>Your Bag</h2>
+          
           <h4 className="empty-cart">is currently empty.</h4>
         </header>
       </section>
