953711f8ab08cca1745e09e1d887520bc3f7f59a
theblaisecode <theblaisecode@gmail.com>
2024-10-17 17:40:03 +0100

    first commit

--- 10 - Comfy Store/src/components/UserTab.jsx
+++ 10 - Comfy Store/src/components/UserTab.jsx
@@ -1,9 +1,12 @@
+import { Link } from "react-router-dom";
+
 function UserTab() {
   return (
     <div className="bg-neutral py-2 px-4 text-neutral-content">
       <div className="container">
-        <div className="userTab flex justify-end">
-          Sign in / Guest &nbsp; Create Account
+        <div className="userTab flex justify-center gap-5 text-sm md:justify-end">
+          <Link to="login">Sign in / Guest</Link>
+          <Link to="register"> Create Account</Link>
         </div>
       </div>
     </div>
