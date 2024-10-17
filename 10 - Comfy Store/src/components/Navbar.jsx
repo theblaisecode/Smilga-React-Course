e9eb2082e0bbcfe8d4d2fc09504b171b97a171da
theblaisecode <theblaisecode@gmail.com>
2024-10-17 18:19:48 +0100

    first commit

--- 10 - Comfy Store/src/components/Navbar.jsx
+++ 10 - Comfy Store/src/components/Navbar.jsx
@@ -1,4 +1,4 @@
-import { NavLink } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 import { IoSunnySharp } from "react-icons/io5";
 import { IoMoon } from "react-icons/io5";
 import { FiShoppingCart } from "react-icons/fi";
@@ -10,48 +10,50 @@ function Navbar() {
   return (
     <header className="bg-base-200">
       <div className="mx-auto w-11/12 lg:w-7/12">
-        <div className="navbar flex justify-between">
-          <div className="logo">
-            <img src={logo} alt="" className="w-8/12" />
+        <div className="navbar flex justify-between items-center">
+          <div className="menu flex justify-between items-center">
+            <Link to="/" className="logo hidden lg:block">
+              <img src={logo} alt="Comfy Stores Logo" className="lg:w-8/12" />
+            </Link>
+
+            <nav className="flex gap-1 flex-col lg:flex-row lg:gap-4">
+              <NavLink
+                to="/"
+                aria-label="Link to Home page"
+                className={({ isActive }) =>
+                  isActive ? "activeStyle p-4" : "p-4"
+                }>
+                Home
+              </NavLink>
+
+              <NavLink
+                to="about"
+                aria-label="Link to About page"
+                className={({ isActive }) =>
+                  isActive ? "activeStyle p-4" : "p-4"
+                }>
+                About
+              </NavLink>
+
+              <NavLink
+                to="products"
+                aria-label="Link to Products page"
+                className={({ isActive }) =>
+                  isActive ? "activeStyle p-4" : "p-4"
+                }>
+                Products
+              </NavLink>
+
+              <NavLink
+                to="cart"
+                aria-label="Link to Cart page"
+                className={({ isActive }) =>
+                  isActive ? "activeStyle p-4" : "p-4"
+                }>
+                Cart
+              </NavLink>
+            </nav>
           </div>
-
-          <nav className="flex gap-4">
-            <NavLink
-              to="/"
-              aria-label="Link to Home page"
-              className={({ isActive }) =>
-                isActive ? "activeStyle p-4" : "p-4"
-              }>
-              Home
-            </NavLink>
-
-            <NavLink
-              to="about"
-              aria-label="Link to About page"
-              className={({ isActive }) =>
-                isActive ? "activeStyle p-4" : "p-4"
-              }>
-              About
-            </NavLink>
-
-            <NavLink
-              to="products"
-              aria-label="Link to Products page"
-              className={({ isActive }) =>
-                isActive ? "activeStyle p-4" : "p-4"
-              }>
-              Products
-            </NavLink>
-
-            <NavLink
-              to="cart"
-              aria-label="Link to Cart page"
-              className={({ isActive }) =>
-                isActive ? "activeStyle p-4" : "p-4"
-              }>
-              Cart
-            </NavLink>
-          </nav>
 
           <div className="theme">
             <IoSunnySharp />
