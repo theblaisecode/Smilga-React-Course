73210144666d8aa7e8e6e6d2afe233cb2d7cab57
theblaisecode <theblaisecode@gmail.com>
2024-10-28 17:59:43 +0100

    first commit

--- 10 - Comfy Store/src/components/product/SingleProduct.jsx
+++ 10 - Comfy Store/src/components/product/SingleProduct.jsx
@@ -1,9 +1,10 @@
 import { Link, useLoaderData } from "react-router-dom";
 import { customFetch, formatPrice } from "../../utils";
-import { useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
-import { changeColor } from "../../reduxToolkit/product/productColor";
+import { changeColor } from "../../reduxToolkit/product/productColorSlice";
+import { isItemAmount } from "../../reduxToolkit/product/productItemAmountSlice";
 
+// Fetch single product data
 export const singleProductLoader = async ({ params }) => {
   const res = await customFetch(`products/${params.id}`);
   return { product: res.data.data };
@@ -11,13 +12,13 @@ export const singleProductLoader = async ({ params }) => {
 
 function SingleProduct() {
   const { product } = useLoaderData();
-  const { image, title, price, description, colors, company } =
-    product.attributes;
+  const { image, title, price, description, colors, company } = product.attributes;
   const dollarsAmount = formatPrice(price);
-  const dispatch = useDispatch();
-  const productColor = useSelector((state) => state.color.color) || colors[0];
 
-  const [amount, setAmount] = useState(1);
+  // Redux selectors and dispatch
+  const { color } = useSelector((state) => state.color) || { color: colors[0] };
+  const { itemAmount } = useSelector((state) => state.itemAmount);
+  const dispatch = useDispatch();
 
   return (
     <>
@@ -39,13 +40,11 @@ function SingleProduct() {
           className="w-96 h-96 object-cover rounded-lg lg:w-full"
         />
 
-        <div className="">
+        <div>
           <h1 className="capitalize text-3xl font-bold">{title}</h1>
-
           <h4 className="text-xl text-neutral-content font-bold mt-2">
             {company}
           </h4>
-
           <p className="mt-3 text-xl">{dollarsAmount}</p>
           <p className="mt-6 leading-8">{description}</p>
 
@@ -53,17 +52,16 @@ function SingleProduct() {
             <h4 className="text-md font-medium tracking-wider capitalize">
               colors
             </h4>
-
             <div className="mt-2">
-              {colors.map((color) => (
+              {colors.map((colorOption) => (
                 <button
-                  key={color}
+                  key={colorOption}
                   type="button"
                   className={`badge w-6 h-6 mr-2 ${
-                    color === productColor && "border-2 border-secondary"
+                    colorOption === color && "border-2 border-secondary"
                   }`}
-                  style={{ backgroundColor: color }}
-                  onClick={() => dispatch(changeColor(color))}></button>
+                  style={{ backgroundColor: colorOption }}
+                  onClick={() => dispatch(changeColor(colorOption))}></button>
               ))}
             </div>
 
@@ -76,8 +74,10 @@ function SingleProduct() {
               <select
                 className="select select-secondary select-bordered select-md"
                 id="amount"
-                value={amount}
-                onChange={(e) => setAmount(parseInt(e.target.value))}>
+                value={itemAmount}
+                onChange={(e) =>
+                  dispatch(isItemAmount(parseInt(e.target.value)))
+                }>
                 {[...Array(20).keys()].map((i) => (
                   <option key={i + 1} value={i + 1}>
                     {i + 1}
@@ -85,6 +85,7 @@ function SingleProduct() {
                 ))}
               </select>
             </div>
+
             <div className="mt-10">
               <button className="btn btn-secondary btn-md uppercase">
                 Add to bag
--- 10 - Comfy Store/src/reduxToolkit/store.js
+++ 10 - Comfy Store/src/reduxToolkit/store.js
@@ -1,12 +1,14 @@
 import { configureStore } from "@reduxjs/toolkit";
 import mobileReducer from "../reduxToolkit/mobileMenu/mobileMenuSlice";
 import themeReducer from "../reduxToolkit/darkMode/darkModeSlice";
-import colorReducer from "./product/productColor";
+import colorReducer from "./product/productColorSlice";
+import itemAmountReducer from "./product/productItemAmountSlice";
 
 export const store = configureStore({
   reducer: {
     mobileMenu: mobileReducer,
     themeMode: themeReducer,
     color: colorReducer,
+    itemAmount: itemAmountReducer,
   },
 });
