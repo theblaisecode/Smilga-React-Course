0a754e9ab37e8ca88dcd0fdbec3ed1b509aeb94f
theblaisecode <theblaisecode@gmail.com>
2024-10-30 12:28:14 +0100

    first commit

--- 10 - Comfy Store/src/components/product/ProductList.jsx
+++ 10 - Comfy Store/src/components/product/ProductList.jsx
@@ -13,20 +13,22 @@ function ProductList() {
           <Link
             key={singleProduct.id}
             to={`/products/${singleProduct.id}`}
-            className="card w-full shadow-xl hover:shadow-2xl transition duration-300">
+            className="p-8 rounded-lg flex flex-col sm:flex-row gap-y-4 flex-wrap  bg-base-100 shadow-xl hover:shadow-2xl duration-300 group">
             <figure className="px-4 pt-4">
               <img
                 src={image}
                 alt={title}
-                className="rounded-xl h-64 md:h-48 w-full object-cover"
+                className="h-24 w-24 rounded-lg sm:h-32 sm:w-32 object-cover group-hover:scale-105 transition duration-300"
               />
             </figure>
 
-            <div className="card-body items-center text-center">
+            <div className="ml-0 sm:ml-16">
               <h2 className="card-title capitalize tracking-wider">{title}</h2>
 
               <span className="text-secondary">{dollarsAmount}</span>
             </div>
+
+            <p className="font-medium ml-0 sm:ml-auto text-lg">$179.99</p>
           </Link>
         );
       })}
