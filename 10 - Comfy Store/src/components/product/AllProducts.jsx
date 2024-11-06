56fcb847cc4efd097290a05e93762e89caaa56ee
theblaisecode <theblaisecode@gmail.com>
2024-11-06 14:08:05 +0100

    first commit

--- 10 - Comfy Store/src/components/product/AllProducts.jsx
+++ 10 - Comfy Store/src/components/product/AllProducts.jsx
@@ -1,25 +1,25 @@
 import { useLoaderData } from "react-router-dom";
+import { useState } from "react";
 import ProductGrid from "./ProductGrid";
 import ProductList from "./ProductList";
 import { BsFillGridFill, BsList } from "react-icons/bs";
-import { useState } from "react";
 
 function AllProducts() {
   const { meta } = useLoaderData();
 
-  const total = meta.pagination.total;
+  const totalProducts = meta.pagination.total;
 
   const [layout, setLayout] = useState("grid");
 
   const setActiveStyles = (pattern) => {
-    return `text-xl btn btn-circle btn-sm`;
+    return `text-xl btn btn-circle btn-sm ${pattern === layout ? "btn-primary text-primary-content" : "btn-ghost text-based-content"}`;
   };
 
   return (
     <>
       <div className="flex justify-between items-center mt-8 border-b border-base-300 pb-5">
         <h4 className="font-medium text-md">
-          {total} Product{total > 1 ? "s" : ""}
+          {totalProducts} Product{totalProducts > 1 ? "s" : ""}
         </h4>
 
         <div className="flex gap-x-2">
@@ -40,7 +40,7 @@ function AllProducts() {
       </div>
 
       <div className="">
-        {total === 0 ? (
+        {totalProducts === 0 ? (
           <h5 className="text-2xl mt-16">
             Sorry, no products match your search...
           </h5>
