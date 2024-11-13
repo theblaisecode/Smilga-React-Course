658dfd693e25b1b458a00cb5729609c61914055e
theblaisecode <theblaisecode@gmail.com>
2024-11-13 12:02:23 +0100

    first commit

--- 10 - Comfy Store/src/components/cart/CartItem.jsx
+++ 10 - Comfy Store/src/components/cart/CartItem.jsx
@@ -1,6 +1,30 @@
-function CartItem() {
+function CartItem({ cartItem }) {
+  const { cartID, title, price, image, amount, company, color } = cartItem;
+
   return (
-    <h1 className="text-3xl">CartItem</h1>
-  )
+    <article
+      key={cartID}
+      className="mb-12 flex lex-col gap-y-4 sm:flex-row flex-wrap border-b border-base-300 pb-6 last:border-b-0">
+      <img
+        src={image}
+        alt={title}
+        className="h-24 w-24 rounded sm:h-32 sm:w-32 object-cover"
+      />
+
+      <div className="sm:ml-16">
+        <h3 className="capitalize font-medium">{title}</h3>
+
+        <h4 className="mt-2 capitalize text-sm text-secondary">{company}</h4>
+
+        <p className="mt-4 text-sm capitalize flex items-center gap-x-2">
+          color:
+          <span
+            className="badge badge-sm"
+            style={{ backgroundColor: color }}></span>
+        </p>
+      </div>
+    </article>
+  );
 }
-export default CartItem
+
+export default CartItem;
--- 10 - Comfy Store/src/components/cart/CartItemsList.jsx
+++ 10 - Comfy Store/src/components/cart/CartItemsList.jsx
@@ -1,6 +1,15 @@
+import { useSelector } from "react-redux";
+import CartItem from "./CartItem";
+
 function CartItemsList() {
+  const cartItems = useSelector((state) => state.cartState.cartItems);
+
   return (
-    <h1 className="text-3xl">CartItemsList</h1>
-  )
+    <>
+      {cartItems.map((item) => {
+        return <CartItem key={item.cartID} cartItem={item} />;
+      })}
+    </>
+  );
 }
-export default CartItemsList
+export default CartItemsList;
