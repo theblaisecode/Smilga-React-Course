c2ca02120a4363423d05072fc97955b147a57766
theblaisecode <theblaisecode@gmail.com>
2024-11-14 14:42:13 +0100

    first commit

--- 10 - Comfy Store/src/components/UserTab.jsx
+++ 10 - Comfy Store/src/components/UserTab.jsx
@@ -1,18 +1,43 @@
-import { Link } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { Link, useNavigate } from "react-router-dom";
+import { clearCart } from "../reduxToolkit/cart/cartSlice";
+import { logoutUser } from "../reduxToolkit/user/userSlice";
 
 function UserTab() {
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const user = useSelector((state) => state.userState.user);
+
+  const userLogout = () => {
+    navigate("/");
+    dispatch(clearCart());
+    dispatch(logoutUser());
+  };
+
   return (
     <div className="bg-neutral py-3 px-4 text-neutral-content">
       <div className="mx-auto max-w-6xl px-8">
-        <div className="userTab flex justify-center gap-5 text-sm md:justify-end">
-          <Link to="login" className="hover:underline">
-            Sign in / Guest
-          </Link>
+        {user ? (
+          <div className="userTab flex justify-center gap-5 text-sm md:justify-end flex gap-x-2 sm:gap-x-8 items-center">
+            <p className="text-sm sm:text-sm">Hello, {user.username}</p>
 
-          <Link to="register" className="hover:underline">
-            Create Account
-          </Link>
-        </div>
+            <button
+              className="btn btn-xs btn-outline btn-primary"
+              onClick={userLogout}>
+              logout
+            </button>
+          </div>
+        ) : (
+          <div className="userTab flex justify-center gap-5 text-sm md:justify-end">
+            <Link to="login" className="hover:underline">
+              Sign in / Guest
+            </Link>
+
+            <Link to="register" className="hover:underline">
+              Create Account
+            </Link>
+          </div>
+        )}
       </div>
     </div>
   );
