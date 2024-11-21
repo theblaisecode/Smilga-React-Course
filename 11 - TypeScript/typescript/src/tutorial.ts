26a09f0709eb4282725fdebd2e5d57a549989844
Blaise Nwachukwu <89015653+theblaisecode@users.noreply.github.com>
2024-11-21 10:20:34 +0100

    first commit

--- 11 - TypeScript/typescript/src/tutorial.ts
+++ 11 - TypeScript/typescript/src/tutorial.ts
@@ -1,1 +1,21 @@
 console.log("TypeScript Tutorial");
+
+interface someValues {
+  name: string;
+  age: number;
+  isFemale: boolean;
+}
+
+let sampleObject: someValues = {
+  name: "Blaise",
+  age: 10 + 2,
+  isFemale: true
+};
+
+// let sampleObject: someValues = {
+//   name: 6 + 7,
+//   age: 'sam',
+//   isFemale: null
+// };
+
+console.log(sampleObject)
