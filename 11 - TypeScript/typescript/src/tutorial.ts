50277b9434604a3c7981c568970bc05f80c3eefb
Blaise Nwachukwu <89015653+theblaisecode@users.noreply.github.com>
2024-11-22 16:45:35 +0100

    first commit

--- 11 - TypeScript/typescript/src/tutorial.ts
+++ 11 - TypeScript/typescript/src/tutorial.ts
@@ -1,16 +1,16 @@
-console.log("TypeScript Tutorial");
+// console.log("TypeScript Tutorial");
 
-interface someValue {
-  name: string;
-  age: number;
-  isFemale: boolean;
-}
+// interface someValue {
+//   name: string;
+//   age: number;
+//   isFemale: boolean;
+// }
 
-let sampleObject: someValue = {
-  name: "Blaise",
-  age: 10 + 2,
-  isFemale: true,
-};
+// let sampleObject: someValue = {
+//   name: "Blaise",
+//   age: 10 + 2,
+//   isFemale: true,
+// };
 
 // let sampleObject: someValue = {
 //   name: 6 + 7,
@@ -18,16 +18,31 @@ let sampleObject: someValue = {
 //   isFemale: null
 // };
 
-console.log(sampleObject);
+// console.log(sampleObject);
 
-// 1. String
-let greeting: string = "Hello everyone!";
-console.log(greeting.toUpperCase());
+// // 1. String
+// let greeting: string = "Hello everyone!";
+// console.log(greeting.toUpperCase());
 
-// 2. Number
-let age: number = 12;
-console.log(age + 8);
+// // 2. Number
+// let age: number = 12;
+// console.log(age + 8);
 
-// 3. Boolean
-let isFemale: boolean = true;
-console.log(isFemale ? "Yay! she's female" : "She isn't female");
+// // 3. Boolean
+// let isFemale: boolean = true;
+// console.log(isFemale ? "Yay! she's female" : "She isn't female");
+
+// ------------------------
+// Union | any
+// ------------------------
+
+let orderStatus: "processing" | "shipped" | "delivered" = "processing";
+orderStatus = "shipped";
+orderStatus = "delivered";
+
+console.log(orderStatus);
+
+let discount: number | string = 20;
+discount = "20%";
+
+console.log(discount);
