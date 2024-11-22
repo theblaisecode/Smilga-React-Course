56aeaa25fa8620b7d8b974d4bff27d74dc0f6cf1
Blaise Nwachukwu <89015653+theblaisecode@users.noreply.github.com>
2024-11-22 17:19:15 +0100

    first commit

--- 11 - TypeScript/typescript/src/tutorial.ts
+++ 11 - TypeScript/typescript/src/tutorial.ts
@@ -60,3 +60,28 @@
 
 let car: { brand: string; year: number } = { brand: "Zepher", year: 3045 };
 console.log(car);
+
+/**
+ * 
+ *  Create an object bike of type { brand: string, year: number } and assign it some values. Then, try to assign a string to the year property.
+- Create an object laptop of type { brand: string, year: number } and try to assign an object with missing year property to it.
+- Create an array products of type { title: string, price?: number }[] and assign it some values. Then, try to add an object with a price property of type string to it.
+ */
+
+// let bike: { brand: string; year: number } = {
+//   brand: "Kawasaki Ninja H2r",
+//   year: 2024,
+// };
+// bike.year = "1996";
+
+// let laptop: { brand: string; year: number } = { brand: "Asus ROG", year: 2023 };
+// let laptop2: { brand: string; year: number } = { brand: "Asus ROG" };
+
+// console.log(laptop, laptop2);
+
+// let candy = { title: "Lemon Plus", price: 10 };
+// let protein = { title: "Chicken", price: 5500 };
+// let bedding = { title: "Pillow", };
+
+let products: {title: string; price: number}[] = [candy, protein, bedding]
+console.log(products)
