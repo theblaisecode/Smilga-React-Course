160a6bde79db744f7c822c381b9c96d9c08f96b1
Blaise Nwachukwu <89015653+theblaisecode@users.noreply.github.com>
2024-11-26 07:51:03 +0100

    first commit

--- 11 - TypeScript/typescript/src/tutorial.ts
+++ 11 - TypeScript/typescript/src/tutorial.ts
@@ -156,25 +156,21 @@ console.log(processData("Blaise", { reverse: true }));
 // ------------------------
 // Type Alias | Intersection Type
 // ------------------------
+type User = { id: number; name: string; isActive: boolean };
 
-const john: { id: number; name: string; isActive: boolean } = {
+const john: User = {
   id: 1,
   name: "john",
   isActive: true,
 };
 
-const susan: { id: number; name: string; isActive: boolean } = {
+const susan: User = {
   id: 1,
   name: "susan",
   isActive: false,
 };
 
-function createUser(user: { id: number; name: string; isActive: boolean }): {
-  id: number;
-  name: string;
-  isActive: boolean;
-} {
+function createUser(user: User): User {
   console.log(`Hello there ${user.name.toUpperCase()} !!!`);
-
   return user;
 }
