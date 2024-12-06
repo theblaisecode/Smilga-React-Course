fd9531ed0a30237be680a8879fa5eda070deeb29
Blaise Nwachukwu <89015653+theblaisecode@users.noreply.github.com>
2024-12-06 07:00:34 +0100

    first commit

--- 11 - TypeScript/typescript/src/tutorial.ts
+++ 11 - TypeScript/typescript/src/tutorial.ts
@@ -391,3 +391,32 @@
 // const user: User = { name: "Blaise", status: statusVaue as Status };
 
 // Type Unknown
+let unknownValue: unknown;
+
+unknownValue = "Olla!";
+unknownValue = [3, 2, 1, "hello"];
+unknownValue = 47.0989765;
+
+if (typeof unknownValue === "number") {
+  unknownValue.toFixed(2);
+}
+
+function runSomeCode() {
+  const random = Math.random();
+
+  if (random < 0.5) {
+    throw new Error("there was an error...");
+  } else {
+    throw "some error";
+  }
+}
+
+try {
+  runSomeCode();
+} catch (error) {
+  if (error instanceof Error) {
+    console.log(error.message);
+  } else {
+    console.log(error);
+  }
+}
