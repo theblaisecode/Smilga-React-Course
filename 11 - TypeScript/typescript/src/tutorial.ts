772feb98cf05c33a19d226bcc85eef6abbabb1fa
Blaise Nwachukwu <89015653+theblaisecode@users.noreply.github.com>
2024-12-10 19:11:09 +0100

    first commit

--- 11 - TypeScript/typescript/src/tutorial.ts
+++ 11 - TypeScript/typescript/src/tutorial.ts
@@ -624,15 +624,37 @@
 // Discriminated Unions and exhaustive check using the never type
 
 type IncrementAction = {
+  type: "increment";
   amount: number;
   timestamp: number;
   user: string;
 };
 
 type DecrementAction = {
+  type: "decrement";
   amount: number;
   timestamp: number;
   user: string;
 };
 
 type Action = IncrementAction | DecrementAction;
+
+function reducer(state: number, action: Action): number {
+  switch (action.type) {
+    case "increment":
+      return state + action.amount;
+    case "decrement":
+      return state - action.amount;
+
+    default:
+      const unexpectedAction: never = action;
+      throw new Error(`Unexpected action: ${unexpectedAction}`);
+  }
+}
+
+const newState = reducer(15, {
+  user: "john",
+  type: "increment",
+  amount: 5,
+  timestamp: 123456,
+});
