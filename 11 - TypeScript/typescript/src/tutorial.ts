8b39ce4fa9b84190ae8312c1cd9cf2f142d15b6c
Blaise Nwachukwu <89015653+theblaisecode@users.noreply.github.com>
2024-12-10 18:57:36 +0100

    first commit

--- 11 - TypeScript/typescript/src/tutorial.ts
+++ 11 - TypeScript/typescript/src/tutorial.ts
@@ -589,4 +589,34 @@
 // console.log(someStr);
 
 // Type Predicate
-// 
+
+type Student = {
+  name: string;
+  study: () => void;
+};
+
+type User = {
+  name: string;
+  login: () => void;
+};
+
+type Person = Student | User;
+
+const randomPerson = (): Person => {
+  return Math.random() > 0.5
+    ? { name: "john", study: () => console.log("Studying") }
+    : { name: "mary", login: () => console.log("Logging in") };
+};
+
+const person = randomPerson();
+
+function isStudent(person: Person): person is Student {
+  // return 'study' in person;
+  return (person as Student).study !== undefined;
+}
+
+if (isStudent(person)) {
+  person.study(); // This is safe because TypeScript knows that 'person' is a Student.
+} else {
+  person.login();
+}
