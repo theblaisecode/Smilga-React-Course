ed32b2a22dbbe94c792594bd27a522f9322d8f69
Blaise Nwachukwu <89015653+theblaisecode@users.noreply.github.com>
2024-12-10 19:03:17 +0100

    first commit

--- 11 - TypeScript/typescript/src/tutorial.ts
+++ 11 - TypeScript/typescript/src/tutorial.ts
@@ -590,33 +590,49 @@
 
 // Type Predicate
 
-type Student = {
-  name: string;
-  study: () => void;
+// type Student = {
+//   name: string;
+//   study: () => void;
+// };
+
+// type User = {
+//   name: string;
+//   login: () => void;
+// };
+
+// type Person = Student | User;
+
+// const randomPerson = (): Person => {
+//   return Math.random() > 0.5
+//     ? { name: "john", study: () => console.log("Studying") }
+//     : { name: "mary", login: () => console.log("Logging in") };
+// };
+
+// const person = randomPerson();
+
+// function isStudent(person: Person): person is Student {
+//   // return 'study' in person;
+//   return (person as Student).study !== undefined;
+// }
+
+// if (isStudent(person)) {
+//   person.study(); // This is safe because TypeScript knows that 'person' is a Student.
+// } else {
+//   person.login();
+// }
+
+// Discriminated Unions and exhaustive check using the never type
+
+type IncrementAction = {
+  amount: number;
+  timestamp: number;
+  user: string;
 };
 
-type User = {
-  name: string;
-  login: () => void;
+type DecrementAction = {
+  amount: number;
+  timestamp: number;
+  user: string;
 };
 
-type Person = Student | User;
-
-const randomPerson = (): Person => {
-  return Math.random() > 0.5
-    ? { name: "john", study: () => console.log("Studying") }
-    : { name: "mary", login: () => console.log("Logging in") };
-};
-
-const person = randomPerson();
-
-function isStudent(person: Person): person is Student {
-  // return 'study' in person;
-  return (person as Student).study !== undefined;
-}
-
-if (isStudent(person)) {
-  person.study(); // This is safe because TypeScript knows that 'person' is a Student.
-} else {
-  person.login();
-}
+type Action = IncrementAction | DecrementAction;
