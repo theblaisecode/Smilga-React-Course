f03c1671acb87f6253ae6dea1e4f918815c6fcf9
Blaise Nwachukwu <89015653+theblaisecode@users.noreply.github.com>
2024-12-14 23:18:11 +0100

    first commit

--- 11 - TypeScript/typescript/src/tutorial.ts
+++ 11 - TypeScript/typescript/src/tutorial.ts
@@ -661,8 +661,12 @@
 // ------------------------
 // Generics - Fundamentals
 // ------------------------
-let array1: string[] = ["Apple", "Banana", "Mango"];
-let array2: number[] = [1, 2, 3];
-let array3: boolean[] = [true, false, true];
+// let array1: string[] = ["Apple", "Banana", "Mango"];
+// let array2: number[] = [1, 2, 3];
+// let array3: boolean[] = [true, false, true];
+
+// let array1: Array<string> = ["Apple", "Banana", "Mango"];
+// let array2: Array<number> = [1, 2, 3];
+// let array3: Array<boolean> = [true, false, true];
 
 console.log(array1, array2, array3);
