cd29c0877a17f4a1bb17e90276091a89caa86302
Blaise Nwachukwu <89015653+theblaisecode@users.noreply.github.com>
2024-12-16 19:25:03 +0100

    first commit

--- 11 - TypeScript/typescript/src/tutorial.ts
+++ 11 - TypeScript/typescript/src/tutorial.ts
@@ -803,21 +803,21 @@
 
 
 
-const { data } = axios.get(someUrl);
+// const { data } = axios.get(someUrl);
 
-axios.get<{ name: string }[]>(someUrl);
+// axios.get<{ name: string }[]>(someUrl);
 
-export class Axios {
-  get<T = any, R = AxiosResponse<T>, D = any>(
-    url: string,
-    config?: AxiosRequestConfig<D>
-  ): Promise<R>;
-}
+// export class Axios {
+//   get<T = any, R = AxiosResponse<T>, D = any>(
+//     url: string,
+//     config?: AxiosRequestConfig<D>
+//   ): Promise<R>;
+// }
 
-export interface AxiosResponse<T = any, D = any> {
-  data: T;
-  status: number;
-  statusText: string;
-  headers: RawAxiosResponseHeaders | AxiosResponseHeaders;
-  config: InternalAxiosRequestConfig<D>;
-  request?: any;
+// export interface AxiosResponse<T = any, D = any> {
+//   data: T;
+//   status: number;
+//   statusText: string;
+//   headers: RawAxiosResponseHeaders | AxiosResponseHeaders;
+//   config: InternalAxiosRequestConfig<D>;
+//   request?: any;
