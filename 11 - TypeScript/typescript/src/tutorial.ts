610e775021387a831456afc17c648439cb440ee8
Blaise Nwachukwu <89015653+theblaisecode@users.noreply.github.com>
2024-12-16 19:23:31 +0100

    first commit

--- 11 - TypeScript/typescript/src/tutorial.ts
+++ 11 - TypeScript/typescript/src/tutorial.ts
@@ -785,3 +785,39 @@
 // printName(student);
 // printName(product);
 // printName(car);
+
+// interface StoreData<T = any> {
+//   data: T[];
+// }
+
+// const storeNumbers: StoreData<number> = {
+//   data: [1, 2, 3, 4],
+// };
+
+// const randomStuff: StoreData = {
+//   data: ["random", 1],
+// };
+
+// console.log(storeNumbers);
+// console.log(randomStuff);
+
+
+
+const { data } = axios.get(someUrl);
+
+axios.get<{ name: string }[]>(someUrl);
+
+export class Axios {
+  get<T = any, R = AxiosResponse<T>, D = any>(
+    url: string,
+    config?: AxiosRequestConfig<D>
+  ): Promise<R>;
+}
+
+export interface AxiosResponse<T = any, D = any> {
+  data: T;
+  status: number;
+  statusText: string;
+  headers: RawAxiosResponseHeaders | AxiosResponseHeaders;
+  config: InternalAxiosRequestConfig<D>;
+  request?: any;
