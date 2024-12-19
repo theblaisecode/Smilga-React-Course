9d12eccd6d0492a33b0beb63867e17e74845b662
Blaise Nwachukwu <89015653+theblaisecode@users.noreply.github.com>
2024-12-19 21:06:56 +0100

    first commit

--- 11 - TypeScript/typescript/src/tasks.ts
+++ 11 - TypeScript/typescript/src/tasks.ts
@@ -1,3 +1,4 @@
 const taskForm = document.querySelector<HTMLFormElement>(".form");
 const formInput = document.querySelector<HTMLInputElement>(".form-input");
 const taskListElement = document.querySelector<HTMLUListElement>(".list");
+console.log(taskForm)
