9a0459f854e872a0d1f73a7463a3b761a2ac28ff
Blaise Nwachukwu <89015653+theblaisecode@users.noreply.github.com>
2025-01-06 14:15:31 +0100

    first commit

--- 11 - TypeScript/typescript/src/tasks.ts
+++ 11 - TypeScript/typescript/src/tasks.ts
@@ -14,9 +14,31 @@ taskForm?.addEventListener("submit", (event) => {
   const taskDescription = formInput?.value;
 
   if (taskDescription) {
+    const task: Task = {
+      description: taskDescription,
+      isCompleted: false,
+    };
+
+    //add task
+    addTask(task)
+
+    //render task
+    renderTask(task)
+
+    //update local storage
     formInput.value = "";
     return;
   }
 
   alert("Please enter a task description");
 });
+
+function addTask(task: Task): void {
+  tasks.push(task);
+}
+
+function renderTask(task:Task): void{
+  const taskElement = document.createElement('li')
+  taskElement.textContent = task.description
+  taskListElement?.appendChild(taskElement)
+}
