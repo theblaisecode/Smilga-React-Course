ad4aafd40e1790104295da2b276566960967eeea
Blaise Nwachukwu <89015653+theblaisecode@users.noreply.github.com>
2025-01-08 07:39:14 +0100

    first commit

--- 12 - TypeScript React/src/starter/02-props/index.tsx
+++ 12 - TypeScript React/src/starter/02-props/index.tsx
@@ -1,8 +1,23 @@
-function Component({ name, id }: { name: string; id: number }):JSX.Element {
+// function Component({ name, id }: { name: string; id: number }):JSX.Element {
+//   return (
+//     <div>
+//       <h2>Name: {name}</h2>
+//       <h2>ID: {id} </h2>
+//     </div>
+//   );
+// }
+// export default Component;
+
+// OR
+
+type ComponentProp = { name: string; id: number };
+
+// function Component({ name, id }: ComponentProp): JSX.Element {
+function Component(props: ComponentProp): JSX.Element {
   return (
     <div>
-      <h2>Name: {name}</h2>
-      <h2>ID: {id} </h2>
+      <h2>Name: {props.name}</h2>
+      <h2>ID: {props.id} </h2>
     </div>
   );
 }
