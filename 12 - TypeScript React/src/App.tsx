bf18bb8fbaf1c0840fc4d1a0f2e83ee4c54b876a
Blaise Nwachukwu <89015653+theblaisecode@users.noreply.github.com>
2025-01-09 16:13:08 +0100

    first commit

--- 12 - TypeScript React/src/App.tsx
+++ 12 - TypeScript React/src/App.tsx
@@ -1,13 +1,15 @@
 // import Component from "./starter/01-return";
-import Component from "./starter/02-props";
+// import Component from "./starter/02-props";
+import Component from "./starter/03-state";
 
 function App() {
   return (
     <main>
-      <Component name="Blaise N." id={23401}>
+      {/* <Component name="Blaise N." id={23401}>
         <h3>Hello World!</h3>
       </Component>
-      <Component name="Teddy N." id={23401} />
+      <Component name="Teddy N." id={23401} /> */}
+      <Component />
     </main>
   );
 }
