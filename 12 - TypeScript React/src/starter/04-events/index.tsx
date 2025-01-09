634feb165d5b540ad5e4e84d5e8721d0fa81b360
Blaise Nwachukwu <89015653+theblaisecode@users.noreply.github.com>
2025-01-09 18:51:16 +0100

    first commit

--- 12 - TypeScript React/src/starter/04-events/index.tsx
+++ 12 - TypeScript React/src/starter/04-events/index.tsx
@@ -5,15 +5,22 @@ function Component() {
   const [email, setEmail] = useState("");
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    console.log(e.target.value);
     setEmail(e.target.value);
   };
+
+  function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+    const formData = new FormData(e.currentTarget);
+    // const formData = new FormData(e.target as HTMLFormElement)
+    const data = Object.fromEntries(formData);
+    console.log(data);
+  }
 
   return (
     <section>
       <h2>React & Typescript</h2>
       <h2>Events Example</h2>
-      <form className="form" action="">
+      <form onSubmit={handleSubmit} className="form" action="">
         <div>
           <label htmlFor="text">Text</label>
           <input
