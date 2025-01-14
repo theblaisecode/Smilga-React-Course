119d94b3717a47cc84aa72322b05a0e7817f5a6b
Blaise Nwachukwu <89015653+theblaisecode@users.noreply.github.com>
2025-01-14 11:30:03 +0100

    first commit

--- 12 - TypeScript React/src/starter/05-challenge/index.tsx
+++ 12 - TypeScript React/src/starter/05-challenge/index.tsx
@@ -12,7 +12,7 @@ type Advanced = {
 type cardType = Basic | Advanced;
 
 function Component(props: cardType) {
-  const { type, name,email } = props;
+  const { type, name, email } = props;
 
   if (type === "basic") {
     return (
@@ -22,24 +22,12 @@ function Component(props: cardType) {
     );
   } else {
     return (
-      <div className="alert alert-success">
+      <div className="alert alert-danger">
         <h3>User : {name}</h3>
         <h3>Email : {email}</h3>
-
       </div>
     );
   }
-
-  return (
-    <div>
-      <div
-        className={
-          type === "basic" ? "alert alert-success" : "alert alert-danger"
-        }>
-        <h3>User : {name}</h3>
-      </div>
-    </div>
-  );
 }
 
 export default Component;
