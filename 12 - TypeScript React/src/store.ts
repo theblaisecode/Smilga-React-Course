68349379d4dfb2a46ea7bdc795ce25bd9c0c2aa5
Blaise Nwachukwu <89015653+theblaisecode@users.noreply.github.com>
2025-01-18 07:47:13 +0100

    first commit

---
+++ 12 - TypeScript React/src/store.ts
@@ -0,0 +1,12 @@
+import { configureStore } from "@reduxjs/toolkit";
+import counterReducer from "./starter/09-rtk/counterSlice";
+// import { counterSlice } from "./starter/09-rtk/counterSlice";
+
+export const store = configureStore({
+  reducer: {
+    counter: counterReducer,
+  },
+});
+
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
